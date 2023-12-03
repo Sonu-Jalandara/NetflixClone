e337828df28d9dab0ebc8700dd1c106c86c0b00d
Sonu-Jalandara <125294281+Sonu-Jalandara@users.noreply.github.com>
2023-12-03 03:46:36 -0500

    added netflix.js

---
+++ netflix.js
@@ -0,0 +1,42 @@
+let listlanguage = document.getElementById("list-lang")
+let languagedrop = document.getElementById("mydropdown")
+listlanguage.addEventListener('click', () => {
+    languagedrop.classList.toggle('active');
+  
+})
+let listlanguage2 = document.getElementById("list-lang2")
+let languagedrop2 = document.getElementById("mydropdown2")
+listlanguage2.addEventListener('click', () => {
+    languagedrop2.classList.toggle('active');
+})
+
+let question1 = document.getElementById("Question1")
+let Answer1 = document.getElementById("answer1")
+question1.addEventListener('click', () => {
+    Answer1.classList.toggle('active');
+})
+let question2 = document.getElementById("Question2")
+let Answer2 = document.getElementById("answer2")
+question2.addEventListener('click', () => {
+    Answer2.classList.toggle('active');
+})
+let question3 = document.getElementById("Question3")
+let Answer3 = document.getElementById("answer3")
+question3.addEventListener('click', () => {
+    Answer3.classList.toggle('active');
+})
+let question4 = document.getElementById("Question4")
+let Answer4 = document.getElementById("answer4")
+question4.addEventListener('click', () => {
+    Answer4.classList.toggle('active');
+})
+let question5 = document.getElementById("Question5")
+let Answer5 = document.getElementById("answer5")
+question5.addEventListener('click', () => {
+    Answer5.classList.toggle('active');
+})
+let question6 = document.getElementById("Question6")
+let Answer6 = document.getElementById("answer6")
+question6.addEventListener('click', () => {
+    Answer6.classList.toggle('active');
+})
